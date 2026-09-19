d692fca7c569e26c6dc9f03c72c7997fad0e6aae
Helen-42 <hahonec1@boe.mono.k12.wv.us>
2026-09-19 17:56:11 -0400

    map info
    mapview models, templates, urls, and views

---
+++ mapview/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Stream(models.Model):
+    name = models.charField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    ph = models.FloatField(null=True, blank=True)
+    """temperature = models.FloatField(null=True, blank=True)
+    dissolved_oxygen = models.FloatField(null=True, blank=True)
+    turbidity = models.FloatField(null=True, blank=True)"""
+
+    def __str__(self)
+        return self.name
+
+    
--- mapview/views.py
+++ mapview/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import Stream
 
 def map_page(request):
     return render(request, "mapview/map.html")
+
+def sensor_data(request):
+    
+    """returns SQL column"""
+    """IF sql formatting follows"""
+    sensor = request.GET.get("sensor", "ph") 
+
+    streams = Stream.objects.all()
+
+    data = []
