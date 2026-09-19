e2d514fd054c1e3aff2ffa445baf86095ab94f75
Helen-42 <hahonec1@boe.mono.k12.wv.us>
2026-09-19 18:29:28 -0400

    map edits
    editing map- NEED to update with sql info when available

--- mapview/views.py
+++ mapview/views.py
@@ -15,3 +15,30 @@ def sensor_data(request):
     streams = Stream.objects.all()
 
     data = []
+
+    for stream in streams:
+
+        if sensor == "ph":
+            value = stream.ph
+
+        elif sensor == "temperature":
+            value = stream.temperature
+
+        elif sensor == "dissolved_oxygen":
+            value = stream.dissolved_oxygen
+
+        elif sensor == "turbidity":
+            value = stream.turbidity
+
+        else:
+            value = None
+
+        if value is not None:
+            data.append({
+                "name": stream.name,
+                "latitude": stream.latitude,
+                "longitude": stream.longitude,
+                "value": value
+            })
+
+    return JsonResponse(data, safe=False)
