e276afaef3fb245644848637fe285fca1fd98bbb
Helen-42 <hahonec1@boe.mono.k12.wv.us>
2026-09-19 21:08:53 -0400

    changing colors

--- hackmit/settings.py
+++ hackmit/settings.py
@@ -7,6 +7,12 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 SECRET_KEY = "django-insecure-hackmit-2026-local-only-throwaway"
 
 DEBUG = True
